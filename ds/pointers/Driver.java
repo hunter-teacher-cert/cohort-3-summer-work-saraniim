677bd0e4bb1fa9b5a5331c572bb5a4e2dc9319f3
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-07-15 15:11:10 +0000

    basic 1 ll

---
+++ ds/pointers/Driver.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+    public static void main(String[] args) {
+
+	// Fist:
+	// Start here and trace through
+	// the code by hand until the comment
+	// that says stop tracing
+
+	Node front;
+	front = new Node("one");
+	front.setNext(new Node("two"));
+	front.getNext().setNext(new Node("three"));
+	front.getNext().getNext().setNext(new Node("four"));
+
+	System.out.println(front.getData());
+
+	Node walker;
+
+	walker = front;
+	System.out.println(front.getData());
+	walker = walker.getNext();
+	System.out.println(walker.getData());
+	System.out.println(front.getNext().getData());
+	// stop tracing
+
+	// Now, compile and run the program
+	// using "javac Driver.java Node.java"
+	// and then "java Driver"
+
+	// Once you've run,
+	// complete the program as per the following comments
+	// and compile and run your program again.
+
+	// Add a line to set the walker variable defined above
+	// so that it points to (refers to) the same node as
+	// the variable front points to.
+	walker = front;
+	// Uncomment the next line, compile and run
+      	// to test what you just added
+	System.out.println(walker.getData());
+
+	// Add the code necessary
+	// to set up a loop that
+	// 1. prints out what is in walker.data()
+	// 2. moves walker to point to the next node
+	// 3. loops until walker gets to null
+
+      while(walker != null){
+      System.out.println(walker.getData());
+          walker= walker.getNext();
+      }
+  }
+}
+// walker alone is the Node itself, not the data in the node
+  //folowed what Jihae had the error was having walker.getNext in while statement as opposed to walker alone - but I compiled and ran and it works
+//oh -- YOU'RE RIGHT!! if you're wong then I am strange
+//lolol XD
+
